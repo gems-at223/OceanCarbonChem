1837f04ea3156d11b19defe96da9a1e8412d311e
gems-at223 <at223@ic.ac.uk>
2025-02-16 23:12:37 +0000

    Added 1D model

---
+++ src/1D_model.jl
@@ -0,0 +1,69 @@
+include("QSS.jl")
+using Oceananigans, OceanBioME
+using Oceananigans.Units
+using CairoMakie
+
+
+
+
+grid = RectilinearGrid(topology = (Flat, Flat, Bounded), size = (32, ), x = 1, y = 1, z = (-100, 0))
+
+
+biogeochemistry = Biogeochemistry(DynamicCarbonateChemistry())
+                               
+
+
+# put the model together
+
+model = NonhydrostaticModel(; grid,
+                              biogeochemistry,
+                              closure = ScalarDiffusivity(κ=1e-1))
+
+set!(model, 
+    c₁ = 7.57*10^-6,    # CO₂ (scaled up from 1.5e-5)
+    c₂ = 1.67*10^-2,    # HCO₃⁻ (scaled up from 1.9e-3)
+    c₃ = 3.15*10^-4,    # CO₃²⁻ (scaled up from 2.5e-4)
+    #c₄ = 6.31*10^-9,   # H⁺ (scaled up from 3.16e-8)
+    c₅ = 9.60*10^-6,   # OH⁻ (scaled up from 3.16e-7)
+    c₆ = 2.97e-4,   # B(OH)₃ (scaled up from 3.75e-4)
+    c₇ = 1.19e-4    # B(OH)₄⁻ (scaled up from 1.25e-4)
+    )
+# run
+
+simulation = Simulation(model, Δt = 0.0001seconds, stop_time = 0.5seconds)
+
+simulation.output_writers[:tracers] = JLD2OutputWriter(model, model.tracers,
+                                                       filename = "column_np.jld2",
+                                                       schedule = TimeInterval(0.005seconds),
+                                                       overwrite_existing = true)
+
+
+run!(simulation)
+
+N = FieldTimeSeries("column_np.jld2", "c₁")
+P = FieldTimeSeries("column_np.jld2", "c₃")
+#sed = FieldTimeSeries("column_np_sediment.jld2", "N_storage")
+
+fig = Figure()
+
+axN = Axis(fig[1, 1], ylabel = "z (m)")
+axP = Axis(fig[2, 1], ylabel = "z (m)")
+#axSed = Axis(fig[3, 1:2], ylabel = "Sediment (mmol N / m²)", xlabel = "Time (years)")
+
+_, _, zc = nodes(grid, Center(), Center(), Center())
+times = N.times
+
+hmN = heatmap!(axN, times , zc, N[1, 1, 1:grid.Nz, 1:end]',
+               interpolate = true, colormap = Reverse(:batlow))
+
+hmP = heatmap!(axP, times, zc, P[1, 1, 1:grid.Nz, 1:end]',
+               interpolate = true, colormap = Reverse(:batlow))
+
+#lines!(axSed, times ./ year, sed[1, 1, 1, :])
+
+Colorbar(fig[1, 2], hmN, label = "Nutrient (mmol N / m³)")
+Colorbar(fig[2, 2], hmP, label = "Phytoplankton (mmol N / m³)")
+
+display(fig)
+
+sleep(10)
---
+++ src/2D_model.jl
@@ -0,0 +1,73 @@
+
+using OceanBioME, Oceananigans
+using Oceananigans.Units
+
+grid = RectilinearGrid(CPU(), size = (160, 32), extent = (10000meters, 500meters), topology = (Bounded, Flat, Bounded))
+
+biogeochemistry = NutrientPhytoplanktonZooplanktonDetritus(; grid) 
+
+model = NonhydrostaticModel(; grid, biogeochemistry,
+                              advection = WENO(; grid),
+                              closure = AnisotropicMinimumDissipation(),
+			      buoyancy = SeawaterBuoyancy(constant_salinity = true))
+
+@inline front(x, z, μ, δ) = μ + δ * tanh((x - 7000 + 4 * z) / 500)
+
+Pᵢ(x, z) = ifelse(z > -50, 0.03, 0.01)
+Nᵢ(x, z) = front(x, z, 2.5, -2)
+Tᵢ(x, z) = front(x, z, 9, 0.05)
+
+set!(model, N = Nᵢ, P = Pᵢ, Z = Pᵢ, T = Tᵢ)
+
+simulation = Simulation(model; Δt = 50, stop_time = 0.5days)
+
+simulation.output_writers[:tracers] = JLD2OutputWriter(model, model.tracers,
+                                                       filename = "buoyancy_front.jld2",
+                                                       schedule = TimeInterval(24minute),
+                                                       overwrite_existing = true)
+
+run!(simulation)
+
+T = FieldTimeSeries("buoyancy_front.jld2", "T")
+N = FieldTimeSeries("buoyancy_front.jld2", "N")
+P = FieldTimeSeries("buoyancy_front.jld2", "P")
+
+xc, yc, zc = nodes(T)
+
+times = T.times
+
+using CairoMakie
+
+n = Observable(1)
+
+T_lims = (8.94, 9.06)
+N_lims = (0, 4.5)
+P_lims = (0.007, 0.02)
+
+Tₙ = @lift interior(T[$n], :, 1, :)
+Nₙ = @lift interior(N[$n], :, 1, :)
+Pₙ = @lift interior(P[$n], :, 1, :)
+
+fig = Figure(size = (1000, 520), fontsize = 20)
+
+title = @lift "t = $(prettytime(times[$n]))"
+Label(fig[0, :], title)
+
+axis_kwargs = (xlabel = "x (m)", ylabel = "z (m)", width = 770, yticks = [-400, -200, 0])
+ax1 = Axis(fig[1, 1]; title = "Temperature (°C)", axis_kwargs...)
+ax2 = Axis(fig[2, 1]; title = "Nutrients concentration (mmol N / m³)",axis_kwargs...)
+ax3 = Axis(fig[3, 1]; title = "Phytoplankton concentration (mmol N / m³)", axis_kwargs...)
+
+hm1 = heatmap!(ax1, xc, zc, Tₙ, colorrange = T_lims, colormap = Reverse(:lajolla), interpolate = true)
+hm2 = heatmap!(ax2, xc, zc, Nₙ, colorrange = N_lims, colormap = Reverse(:bamako), interpolate = true)
+hm3 = heatmap!(ax3, xc, zc, Pₙ, colorrange = P_lims, colormap = Reverse(:bamako), interpolate = true)
+
+Colorbar(fig[1, 2], hm1, ticks = [8.95, 9.0, 9.05])
+Colorbar(fig[2, 2], hm2, ticks = [0, 2, 4])
+Colorbar(fig[3, 2], hm3, ticks = [0.01, 0.02, 0.03])
+
+rowgap!(fig.layout, 0)
+
+record(fig, "buoyancy_front.gif", 1:length(times)) do i
+    n[] = i
+end
