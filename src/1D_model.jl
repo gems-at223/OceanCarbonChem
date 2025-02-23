b869672857e4c736bfd7978874da4c0eb1bcde42
gems-at223 <at223@ic.ac.uk>
2025-02-23 13:51:48 +0000

    Update

--- src/1D_model.jl
+++ src/1D_model.jl
@@ -1,40 +1,99 @@
-include("QSS.jl")
+include("evolutionequations.jl")
+
 using Oceananigans, OceanBioME
 using Oceananigans.Units
+using Oceananigans.BoundaryConditions
+
 using CairoMakie
 
 
 
 
-grid = RectilinearGrid(topology = (Flat, Flat, Bounded), size = (32, ), x = 1, y = 1, z = (-100, 0))
+grid = RectilinearGrid(topology = (Flat, Flat, Bounded), size = (16, ), x = 1, y = 1, z = (-0.01meters, 0meters))
 
 
 biogeochemistry = Biogeochemistry(DynamicCarbonateChemistry())
                                
+CO₂_sink_rate = 1e-4  # Example sink rate value
+
+#function CO₂_sink(z,c₁)
+    #if z > -0.1 && z < -0.05  # Apply sink only in the middle of the domain
+        #return 0
+    #else
+        #return (((-0.05+0.1)*(z-0.05)^2))*(c₁/(5*10^6+c₁))
+    #end
+#end
+#function HCO₃_sink(z,c₁)
+    #if z > -0.1 && z < -0.05  # Apply sink only in the middle of the domain
+     #   return 0
+    #else
+     #   return (((-0.05+0.1)*(z-0.05)^2))*(1-(c₁/(5*10^6+c₁)))
+    #end
+#end
+#function HCO₃_sink(z,c₁)
+    #if z > -0.1125 && z < -0.0875  # Apply sink only in the middle of the domain
+    #    return 0
+    #else
+    #    return ((((-0.0875+0.1125)*(z+0.1)^2)))*(1-(c₁/(5*10^6+c₁)))
+    #end
+#end
+function HCO₃_sink(z,c₁)
+    return ((((-0.02)*(z)^2)))*(1-(c₁/(5*10^6+c₁)))
+end
+
+function CO₂_sink(z,c₁)
+    return ((((-0.02)*(z)^2)))*((c₁/(5*10^6+c₁)))
+
+end
 
 
-# put the model together
+Co2_forcing=Forcing(CO₂_sink, field_dependencies=(:c₁))
+HCO₃_forcing=Forcing(HCO₃_sink, field_dependencies=(:c₁))
+function custom_boundary_condition(z)
+    if z > -0.125 && z < -0.075  # No flux between -0.1 and -0.05
+        return 0.0
+    else
+        return c
+    end
+end
+
+#FieldBoundaryConditions(top = FluxBoundaryCondition(CO₂_flux), bottom = GradientBoundaryCondition(0.0)),
+
+CO₂_flux = 1e-5  # Example flux value
+boundary_conditions = (c₃ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
+                       c₂ = FieldBoundaryConditions(top = FluxBoundaryCondition(CO₂_flux), bottom = GradientBoundaryCondition(0.0)),
+                       c₁ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
+                       c₄ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
+                       c₅ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
+                       c₆ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
+                       c₇ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)))
+biogeochemistry = Biogeochemistry(DynamicCarbonateChemistry())
 
 model = NonhydrostaticModel(; grid,
                               biogeochemistry,
-                              closure = ScalarDiffusivity(κ=1e-1))
+                              closure = ScalarDiffusivity(κ=2e-4),
+                              boundary_conditions = boundary_conditions,
+                              tracers = (:c₁, :c₂, :c₃, :c₄, :c₅, :c₆, :c₇),
+                              #forcing=(;c₁=(CO₂_sink),c₂=(HCO₃_sink))
+                             )
+        
 
 set!(model, 
     c₁ = 7.57*10^-6,    # CO₂ (scaled up from 1.5e-5)
-    c₂ = 1.67*10^-2,    # HCO₃⁻ (scaled up from 1.9e-3)
+    c₂ = 1.67*10^-3,    # HCO₃⁻ (scaled up from 1.9e-3)
     c₃ = 3.15*10^-4,    # CO₃²⁻ (scaled up from 2.5e-4)
-    #c₄ = 6.31*10^-9,   # H⁺ (scaled up from 3.16e-8)
+    c₄ = 6.31*10^-9,   # H⁺ (scaled up from 3.16e-8)
     c₅ = 9.60*10^-6,   # OH⁻ (scaled up from 3.16e-7)
     c₆ = 2.97e-4,   # B(OH)₃ (scaled up from 3.75e-4)
     c₇ = 1.19e-4    # B(OH)₄⁻ (scaled up from 1.25e-4)
     )
 # run
 
-simulation = Simulation(model, Δt = 0.0001seconds, stop_time = 1seconds)
+simulation = Simulation(model, Δt = 0.0000005seconds, stop_time = 0.01seconds)
 
 simulation.output_writers[:tracers] = JLD2OutputWriter(model, model.tracers,
                                                        filename = "column_np.jld2",
-                                                       schedule = TimeInterval(0.005seconds),
+                                                       schedule = TimeInterval(0.0005seconds),
                                                        overwrite_existing = true)
 
 
@@ -42,12 +101,22 @@ run!(simulation)
 
 N = FieldTimeSeries("column_np.jld2", "c₁")
 P = FieldTimeSeries("column_np.jld2", "c₃")
+H= FieldTimeSeries("column_np.jld2", "c₄")
+HCO3= FieldTimeSeries("column_np.jld2", "c₂")
+B= FieldTimeSeries("column_np.jld2", "c₆")
+BOH= FieldTimeSeries("column_np.jld2", "c₇")
+
 #sed = FieldTimeSeries("column_np_sediment.jld2", "N_storage")
 
 fig = Figure()
 
 axN = Axis(fig[1, 1], ylabel = "z (m)")
 axP = Axis(fig[2, 1], ylabel = "z (m)")
+axH = Axis(fig[3, 1], ylabel = "z (m)")
+axHCO3 = Axis(fig[4, 1], ylabel = "z (m)")
+#axB = Axis(fig[5, 1], ylabel = "z (m)")
+#axBOH = Axis(fig[6, 1], ylabel = "z (m)")
+
 #axSed = Axis(fig[3, 1:2], ylabel = "Sediment (mmol N / m²)", xlabel = "Time (years)")
 
 _, _, zc = nodes(grid, Center(), Center(), Center())
@@ -59,13 +128,53 @@ hmN = heatmap!(axN, times , zc, N[1, 1, 1:grid.Nz, 1:end]',
 hmP = heatmap!(axP, times, zc, P[1, 1, 1:grid.Nz, 1:end]',
                interpolate = true, colormap = Reverse(:batlow))
 
+hmH = heatmap!(axH, times, zc, H[1, 1, 1:grid.Nz, 1:end]',
+               interpolate = true, colormap = Reverse(:batlow))
+
+hmHCO3 = heatmap!(axHCO3, times, zc, HCO3[1, 1, 1:grid.Nz, 1:end]',
+                interpolate = true, colormap = Reverse(:batlow))
+
+#hmB = heatmap!(axB, times, zc, B[1, 1, 1:grid.Nz, 1:end]',
+ #              interpolate = true, colormap = Reverse(:batlow))
+
+#hmBOH = heatmap!(axBOH, times, zc, BOH[1, 1, 1:grid.Nz, 1:end]',
+ #                interpolate = true, colormap = Reverse(:batlow))
+
+
+
+
 #lines!(axSed, times ./ year, sed[1, 1, 1, :])
 
 Colorbar(fig[1, 2], hmN, label = "CO₂ (mol/ kg)")
 Colorbar(fig[2, 2], hmP, label = "CO₃ (mol / kg)")
+Colorbar(fig[3, 2], hmH, label = "H⁺ (mol / kg)")
+Colorbar(fig[4, 2], hmHCO3, label = "HCO3⁻ (mol / kg)")
+#Colorbar(fig[5, 2], hmB, label = "B(OH)₃ (mol / kg)")
+#Colorbar(fig[6, 2], hmBOH, label = "B(OH)₄⁻ (mol / kg)")
+
 
 save("1D_model.png",fig)
 
 display(fig)
+
+fig_line = Figure()
+ax_line = Axis(fig_line[1, 1], xlabel = "Length (m)", ylabel = "HCO3⁻ (mol / kg)")
+
+final_time_index = size(HCO3, 4)
+oh_data_final = HCO3[:, :, :, final_time_index]
+
+
+# Extract OH data across the depth of the domain
+oh_profile = oh_data_final[1, 1, 1:16]
+
+println(size(oh_profile))
+println(size(zc))
+
+lines!(ax_line, zc*-1,oh_profile, label = "HCO3⁻ at final time step")
+
+
+save("HCO3_line_plot.png", fig_line)
+
+display(fig_line)
 
 sleep(10)
--- src/2D_model_bio.jl
+++ src/2D_model_bio.jl
@@ -2,14 +2,27 @@
 include("QSS.jl")
 using OceanBioME, Oceananigans
 using Oceananigans.Units
+using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
+using Oceananigans.BoundaryConditions
 
-grid = RectilinearGrid(CPU(), size = (16, 16), extent = (0.0001meters, 0.0001meters), topology = (Bounded, Flat, Bounded))
-
+grid = RectilinearGrid(CPU(), size = (24,24),x = (-0.025meters, 0.025meters), z=(-0.025meters, 0.025meters), topology = (Bounded, Flat, Bounded))
 biogeochemistry = DynamicCarbonateChemistry()
 
-CO₂_flux = 8e-9  # Example flux value
 horizontal_closure = HorizontalScalarDiffusivity(ν=1e-3, κ=2e-9)
 vertical_closure = VerticalScalarDiffusivity(ν=1e-3, κ=2e-9)
+
+CO₂_flux = 1e-5  # Example flux value
+R=0.007
+
+
+
+@inline function inside_cylinder(x, z)
+    return ((x)^2 + (z)^2) <= R^2
+end
+
+
+underlying_grid=grid
+immersed_grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBoundary(inside_cylinder))
 
 boundary_conditions = (c₃ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
                        c₂ = FieldBoundaryConditions(top = FluxBoundaryCondition(CO₂_flux), bottom = GradientBoundaryCondition(0.0)),
@@ -18,14 +31,15 @@ boundary_conditions = (c₃ = FieldBoundaryConditions(top = GradientBoundaryCond
                        c₅ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
                        c₆ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)),
                        c₇ = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)))
-forcing=AdvectiveForcing(w=-0.0000001)
-model = NonhydrostaticModel(; grid, biogeochemistry,
+#forcing=AdvectiveForcing(w=-0.0000001)
+#grid=ImmersedBoundaryGrid(bgrid, boundary)
+
+model = NonhydrostaticModel(; grid=immersed_grid, biogeochemistry,
                              #advection=WENO(;grid),
-                              tracers = ( :c₁, :c₂, :c₃, :c₅, :c₆, :c₇),  
+                              tracers = ( :c₁, :c₂, :c₃, :c₅, :c₆, :c₇),
                               boundary_conditions = boundary_conditions,
-                              #closure = (horizontal_closure, vertical_closure))
-                              closure = ScalarDiffusivity(κ=1e-9))
-                              #forcing=(;c₃=(forcing),c₁ = (forcing))
+                              #closure = (horizontal_closure, vertical_closure)
+                              closure = ScalarDiffusivity(ν=1e-3, κ=5e-6))
 
 @inline front(x, z, μ, δ) = μ + δ * tanh((x - 0.0001 + 0.00004 * z) / 0.00005)
 
@@ -42,7 +56,7 @@ c7ᵢ(x, z) =  1.19e-4
 
 set!(model, c₁=c1ᵢ, c₂=c2i, c₃=c3ᵢ, c₅=c5ᵢ, c₆=c6ᵢ, c₇=c7ᵢ)
 
-simulation = Simulation(model; Δt = 0.0005seconds, stop_time = 1.5seconds)
+simulation = Simulation(model; Δt = 0.0005seconds, stop_time = 1seconds)
 
 simulation.output_writers[:tracers] = JLD2OutputWriter(model, model.tracers,
                                                        filename = "buoyancy_front.jld2",
@@ -63,20 +77,20 @@ using CairoMakie
 
 n = Observable(1)
 
-T_lims = (4.145150960204884e-6 , 7.347156183885458e-6)
-N_lims = ( 0.0008689498744456803 , 0.0016178782670992601)
-P_lims = ( 0.00016390371883256844 , 0.0003051686551714173)
+T_lims = (0.0 , 1.1139999999999998e-5)
+N_lims = ( 0.0 ,0.0025050000000000003)
+P_lims = ( 0.0 , 0.00047250000000000005)
 
 Tₙ = @lift interior(T[$n], :, 1, :)
 Nₙ = @lift interior(N[$n], :, 1, :)
 Pₙ = @lift interior(P[$n], :, 1, :)
 
-fig = Figure(size = (1000, 520), fontsize = 20)
+fig = Figure(size = (1000, 1000), fontsize = 20)
 
 title = @lift "t = $(prettytime(times[$n]))"
 Label(fig[0, :], title)
 
-axis_kwargs = (xlabel = "x (m)", ylabel = "z (m)", width = 770, yticks = [-0.1, -0.05, 0])
+axis_kwargs = (xlabel = "x (m)", ylabel = "z (m)", width = 500, yticks = [-0.025,-0.01,0,0.01,0.025])
 ax1 = Axis(fig[1, 1]; title = "CO2 mol/kg", axis_kwargs...)
 ax2 = Axis(fig[2, 1]; title = "HCO3- mol/kg",axis_kwargs...)
 ax3 = Axis(fig[3, 1]; title = "CO3-2 mol/kg", axis_kwargs...)
